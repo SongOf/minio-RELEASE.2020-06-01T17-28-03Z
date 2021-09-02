fb00917b7d730a740b1b58c85187297cfe9f76e0
songofli <songofli@tencent.com>
2021-09-02 11:11:44 +0800

    test(erasure_test.go): ec vs replication

--- cmd/erasure_test.go
+++ cmd/erasure_test.go
@@ -21,7 +21,9 @@ import (
 	"context"
 	"crypto/rand"
 	"io"
+	"log"
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -43,8 +45,152 @@ var erasureEncodeDecodeTests = []struct {
 	{dataBlocks: 8, parityBlocks: 4, missingData: 2, missingParity: 2, reconstructParity: false, shouldFail: false},
 }
 
+func TestReplicationDisk(t *testing.T) {
+	path := "/Users/lisongsong/Documents/rdata.txt"
+	size := 1 //KB
+	data := make([]byte, 0)
+	fi, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	for i := 0; i < 3; i++ {
+		for i := 0; i < size; i++ {
+			buf := make([]byte, 1024) //每次需要再for循环里面重新make，踩坑
+			n, err := fi.Read(buf)
+			if err != nil && err != io.EOF {
+				panic(err)
+			}
+			if 0 == n {
+				break
+			}
+			data = append(data, buf...)
+		}
+		newPath := path + "-" + strconv.Itoa(i)
+		f, err := os.Create(newPath)
+		defer f.Close()
+		if err != nil {
+			t.Fatalf("Test %d: failed to create erasure: %v", i, err)
+		}
+		_, err = f.Write(data)
+		if err != nil {
+			log.Println("writeFile error ..err =", err)
+			return
+		}
+	}
+}
+func TestECDisk(t *testing.T) {
+	path := "/Users/lisongsong/Documents/rdata.txt"
+	size := 1 //KB
+	data := make([]byte, 0)
+	fi, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	for i := 0; i < size; i++ {
+		buf := make([]byte, 1024) //每次需要再for循环里面重新make，踩坑
+		n, err := fi.Read(buf)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		if 0 == n {
+			break
+		}
+		data = append(data, buf...)
+	}
+
+	for i, test := range erasureEncodeDecodeTests {
+		buffer := make([]byte, len(data), 2*len(data))
+		copy(buffer, data)
+
+		erasure, err := NewErasure(context.Background(), test.dataBlocks, test.parityBlocks, blockSizeV1)
+		if err != nil {
+			t.Fatalf("Test %d: failed to create erasure: %v", i, err)
+		}
+		encoded, err := erasure.EncodeData(context.Background(), buffer)
+		if err != nil {
+			t.Fatalf("Test %d: failed to encode data: %v", i, err)
+		}
+		blocks := len(encoded)
+		for j := 0; j < blocks; j++ {
+			newPath := path + "-" + strconv.Itoa(i) + "-" + strconv.Itoa(j)
+			f, err := os.Create(newPath)
+			defer f.Close()
+			if err != nil {
+				t.Fatalf("Test %d-%d: failed to create erasure: %v", i, j, err)
+			}
+			_, err = f.Write(encoded[j])
+			if err != nil {
+				log.Println("writeFile error ..err =", err)
+				return
+			}
+		}
+		for j := range encoded[:test.missingData] {
+			encoded[j] = nil
+		}
+		for j := test.dataBlocks; j < test.dataBlocks+test.missingParity; j++ {
+			encoded[j] = nil
+		}
+
+		if test.reconstructParity {
+			err = erasure.DecodeDataAndParityBlocks(context.Background(), encoded)
+		} else {
+			err = erasure.DecodeDataBlocks(encoded)
+		}
+
+		if err == nil && test.shouldFail {
+			t.Errorf("Test %d: test should fail but it passed", i)
+		}
+		if err != nil && !test.shouldFail {
+			t.Errorf("Test %d: test should pass but it failed: %v", i, err)
+		}
+
+		decoded := encoded
+		if !test.shouldFail {
+			if test.reconstructParity {
+				for j := range decoded {
+					if decoded[j] == nil {
+						t.Errorf("Test %d: failed to reconstruct shard %d", i, j)
+					}
+				}
+			} else {
+				for j := range decoded[:test.dataBlocks] {
+					if decoded[j] == nil {
+						t.Errorf("Test %d: failed to reconstruct data shard %d", i, j)
+					}
+				}
+			}
+
+			decodedData := new(bytes.Buffer)
+			if _, err = writeDataBlocks(context.Background(), decodedData, decoded, test.dataBlocks, 0, int64(len(data))); err != nil {
+				t.Errorf("Test %d: failed to write data blocks: %v", i, err)
+			}
+			if !bytes.Equal(decodedData.Bytes(), data) {
+				t.Errorf("Test %d: Decoded data does not match original data: got: %v want: %v", i, decodedData.Bytes(), data)
+			}
+		}
+	}
+}
+func TestCreateData(t *testing.T) {
+	data := make([]byte, 1024*1024*1024)
+	if _, err := io.ReadFull(rand.Reader, data); err != nil {
+		t.Fatalf("Failed to read random data: %v", err)
+	}
+	path := "/Users/lisongsong/Documents/rdata.txt"
+	fi, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	_, err = fi.Write(data)
+	if err != nil {
+		log.Println("writeFile error ..err =", err)
+		return
+	}
+}
 func TestErasureEncodeDecode(t *testing.T) {
-	data := make([]byte, 256)
+	data := make([]byte, 1024*1024)
 	if _, err := io.ReadFull(rand.Reader, data); err != nil {
 		t.Fatalf("Failed to read random data: %v", err)
 	}
